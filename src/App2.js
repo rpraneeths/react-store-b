a1edb796ce3b2fbd1ce1c0974f00ec52f2cdb890
rpraneeths <your.email@example.com>
2025-03-07 14:15:18 +0530

    added

---
+++ src/App2.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function App2() {
+    return(
+        <div style={{backgroundColor: 'yellow'}}>App2</div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App3 from './App3';  // Changed App to App1
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode>
+    <App3 />  {/* Using App1 correctly */}
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
